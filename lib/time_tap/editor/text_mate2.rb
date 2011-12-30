29852c259c55ead10af33b51ae8bc43f958b89a3
Elia Schito <perlelia@gmail.com>
2011-12-30 02:09:08 +0100

    Fix TM2 adapter

--- lib/time_tap/editor/text_mate2.rb
+++ lib/time_tap/editor/text_mate2.rb
@@ -1,9 +1,15 @@
-class TimeTap::Editor::TextMate
+class TimeTap::Editor::TextMate2
   def initialize options = {}
   end
   
   def current_path
-    file = File.expand_path("~/.timetap.tm2.current_file")
-    File.exist? file ? File.read(file) : nil
+    container_file = File.expand_path('~/.timetap.tm2.current_file')
+    file = File.read(container_file).strip if File.exist? container_file
+    
+    if File.exist? file
+      file
+    else
+      nil
+    end
   end
 end
